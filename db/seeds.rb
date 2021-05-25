ff08d6c691cca8e3cbec4fdd622a56d04d4078eb
AbsoluteSpace <welsh4568@gmail.com>
2021-05-24 21:42:48 -0700

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,18 @@ User.create(email: "admin@email.com", password: "admin1", password_confirmation:
 
 Product.create(title: "The martian", body:"A scientist is trapped on mars.", tags:"scifi,andy weir,space", price:20.00, file_location:"/assets/aw1")
 Product.create(title: "A short history of nearly everything", body:"Learn about everything.", tags:"history,bill bryson", price:40.00, file_location:"/assets/bb1")
+Product.create(title: "The three-body problem", body:"Contact has been made with aliens.", tags:"scifi,cixin liu,space,aliens", price:30.00, file_location:"/assets/cl1")
+Product.create(title: "The dark forest", body:"Aliens have arrived on Earth.", tags:"scifi,cixin liu,space,aliens", price:30.00, file_location:"/assets/cl2")
+Product.create(title: "Death's end", body:"What happens after aliens arrive on Earth.", tags:"scifi,cixin liu,space,aliens", price:30.00, file_location:"/assets/cl3")
+Product.create(title: "Supernova era", body:"Only kids remain on Earth after a supernova.", tags:"scifi,cixin liu,space", price:30.00, file_location:"/assets/cl4")
+Product.create(title: "The science of interstellar", body:"The science behind the hit movie Interstellar.", tags:"scifi,kip thorne,space", price:20.00, file_location:"/assets/kt1")
+Product.create(title: "The future of humanity", body:"What will civilization be like in the future?", tags:"scifi,michiu kaku,science", price:20.00, file_location:"/assets/mc1")
+Product.create(title: "The future of the mind", body:"What will we be like in the future?", tags:"scifi,michiu kaku,science", price:20.00, file_location:"/assets/mc2")
+Product.create(title: "Checkout", body:"The shopify playbook.", tags:"shopify,checkout", price:100.00, file_location:"/assets/shopify1")
+Product.create(title: "The four-hour body", body:"Get into good shape smarter.", tags:"four hour,tim ferris,self help", price:50.00, file_location:"/assets/tf1")
+Product.create(title: "Tribe of mentors", body:"Learn from the greats.", tags:"tim ferris,self help", price:50.00, file_location:"/assets/tf2")
+Product.create(title: "Tools of the titans", body:"See what the greats do.", tags:"tim ferris,self help", price:50.00, file_location:"/assets/tf3")
+Product.create(title: "The four-hour chef", body:"Get good at cooking.", tags:"four hour,tim ferris,self help", price:50.00, file_location:"/assets/tf4")
+Product.create(title: "The four-hour workweek", body:"Work smarter not harder.", tags:"four hour,tim ferris,self help", price:50.00, file_location:"/assets/tf5")
+
+Discount.create(name: "5 dollars off scifi", percent_discount: false, discount: 5, tags:"scifi", active: false)
